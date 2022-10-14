c97eee46ffca41321a8237bc2f69b384e93c0c80
Jerry Yu <jerryyu01@outlook.com>
2022-10-13 20:56:03 -0700

    updated demo

--- Week4/demo.c
+++ Week4/demo.c
@@ -15,20 +15,32 @@ int main()
 
     // interpret the memory as an signed integer?
     int *test1 = (int *)mem;
-    printf("Interpret memory as signed int: %d.\n", *test1);
+    printf("Interpret memory as signed int: %d (%ld bytes).\n", *test1, sizeof(*test1));
 
     // interpret the memory as a signed long integer?
     long *test2 = (long *)mem;
-    printf("Interpret memory as signed long int: %ld.\n", *test2);
+    printf("Interpret memory as signed long int: %ld (%ld bytes).\n", *test2, sizeof(*test2));
 
     // interpret the memory as a unsigned long integer?
     unsigned long *test3 = (unsigned long *)mem;
-    printf("Interpret memory as signed long int: %lu.\n", *test3);
+    printf("Interpret memory as unsigned long int: %lu (%ld bytes).\n", *test3, sizeof(*test3));
+
+    // interpret the memory as a character?
+    char *test4 = (char *)mem;
+    printf("Interpret memory as char: %c (%ld bytes).\n", *test4, sizeof(*test4));
 
     // interpret the memory as a string?
-    printf("Interpret memory as string: %s.\n", (char *)mem);
+    printf("Interpret memory as string: %s.\n", (char *)test4);
 
     // Is that it?
+
+
+
+
+
+    /*
+     * Pointer arithmetic
+     */
 
     return EXIT_SUCCESS;
 }
